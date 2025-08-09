c844eaf64fce5118797da1544730696cc7b89696
OLOWONISHAYE-SUNKANMI <skprojectx12@gmail.com>
2025-08-09 04:42:59 -0700

    final project

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -23,34 +23,34 @@ export default function settings() {
       </View>
     </View>
 
-      <View>
+      <View style={styles.settingsContainer}>
 
-        <View>
-            <Text>GENERAL</Text>
+        <View style={{ marginBottom: 20 }}>
+            <Text style={styles.textHeader}>GENERAL</Text>
         </View>
 
-        <View>
-             <TouchableOpacity>
+        <View style={styles.settingsList}>
+          <TouchableOpacity style={styles.settingsItem}>
              <Ionicons name="person-circle" size={24} color="#2200ff" />
-             <Text>Profile</Text>
+             <Text style={styles.settingsText}>Profile</Text>
              <Ionicons name="chevron-forward" size={24} color="#2200ff" />
         </TouchableOpacity>
 
-            <TouchableOpacity>
+            <TouchableOpacity style={styles.settingsItem}>
              <Ionicons name="notifications" size={24} color="#2200ff" />
-             <Text>Notification</Text>
+             <Text style={styles.settingsText}>Notification</Text>
              <Ionicons name="chevron-forward" size={24} color="#2200ff" />
         </TouchableOpacity>
 
-            <TouchableOpacity>
+            <TouchableOpacity style={styles.settingsItem}>
              <Ionicons name="log-out" size={24} color="#2200ff" />
-             <Text>Logout</Text>
+             <Text style={styles.settingsText}>Logout</Text>
              <Ionicons name="chevron-forward" size={24} color="#2200ff" />
         </TouchableOpacity>
 
-            <TouchableOpacity>
+            <TouchableOpacity style={styles.settingsItem}>
              <Ionicons name="trash-bin" size={24} color="#2200ff" />
-             <Text>Delete Account</Text>
+             <Text style={styles.settingsText}>Delete Account</Text>
              <Ionicons name="chevron-forward" size={24} color="#2200ff" />
         </TouchableOpacity>
 
@@ -58,22 +58,27 @@ export default function settings() {
         </View>
 
 
-        <View> 
-          <View>
-            <Text>FEEDBACK</Text>
+        <View style={styles.settingsContainer}> 
+          <View style={{ marginBottom: 20, marginTop: 20 }}>
+            <Text style={styles.textHeader}>FEEDBACK</Text>
           </View>
 
-          <TouchableOpacity>
+          <View style={styles.settingsList}>
+              <TouchableOpacity style={styles.settingsItem}>
              <Ionicons name="bug-outline" size={24} color="#2200ff" />
-             <Text>Report a bug</Text>
+             <Text style={styles.settingsText}>Report a bug</Text>
              <Ionicons name="chevron-forward" size={24} color="#2200ff" />
         </TouchableOpacity>
 
-            <TouchableOpacity>
+            <TouchableOpacity style={styles.settingsItem}>
              <Ionicons name="book-outline" size={24} color="#2200ff" />
-             <Text>Send Feedback</Text>
+             <Text style={styles.settingsText}>Send Feedback</Text>
              <Ionicons name="chevron-forward" size={24} color="#2200ff" />
         </TouchableOpacity>
+
+          </View>
+
+        
 
         </View>
 
@@ -102,4 +107,36 @@ const styles = StyleSheet.create({
       fontWeight: 'bold',
       color: '#2200FF',
     },
+    settingsContainer: {
+      marginTop: 20,
+      marginLeft: 10,
+      marginRight: 10,
+    },
+    textHeader: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: '#B3B3B3',
+      marginBottom: 10,
+    },
+    settingsList: {
+      flexDirection: 'column',
+    },
+    settingsItem: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      paddingVertical: 15,
+      paddingHorizontal: 10,
+      borderBottomWidth: 1,
+      borderBottomColor: '#E0E0E0',
+    },
+    settingsText: {
+      fontSize: 18,
+      color: '#2200FF',
+      textAlign: 'left', 
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+      fontWeight: 'bold',  
+    }
+
 })
